d3e5354ac0fce8f56db8c2a3a062c12d064a7595
nadinemaqela-jpg <nadinemaqela@gmail.com>
2026-06-25 14:29:54 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnEx9xLtbac3e0ivdIa7BGuiT3AK3yBb8",
+  authDomain: "aceed-tutoring.firebaseapp.com",
+  projectId: "aceed-tutoring",
+  storageBucket: "aceed-tutoring.firebasestorage.app",
+  messagingSenderId: "225577605291",
+  appId: "1:225577605291:web:75327bc4e1f644cef47fee"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
